a62d9c606f2b0b30e60e47d054e3fa8c1b18c383
Kendell R <KTibow@users.noreply.github.com>
2023-03-16 17:52:32 -0700

    new messages for message logger

--- handlers/resps/funnyMessageLogger.js
+++ handlers/resps/funnyMessageLogger.js
@@ -12,9 +12,22 @@ export const command = async (message) => {
     return;
   if (Math.random() > 0.05 || Date.now() - lastTrigger < 300 * 1000) return;
   lastTrigger = Date.now();
-  const messageText = `Message loggers are so dumb. You're seeing this because you have one.
-Message loggers are unethical, besides breaking the TOS. They also require extra effort from you.
-Please uninstall yours. Using a message logger is just sending a sign that you hate everyone.`;
+  const messageLoggers = [
+    `There once was a logger of text,
+  Whose users were feeling perplexed,
+  For it's unethical ways,
+  And the extra effort it pays,
+  Uninstall it now, what's the pretext?`,
+    `Oh message logger, thou art so uncouth,
+  Unethical and breaking TOS, forsooth!
+  The extra effort required is a pain,
+  Uninstall now, and let it not remain.
+  Sending a sign of hate, oh what a shame,
+  Remove it, and be no longer to blame.`,
+    `Ladies and gentlemen, it is my pleasure to introduce a topic of great importance: message loggers. These tools, while seemingly innocuous, are in fact unethical and a breach of terms of service. Furthermore, their use requires extra effort and sends a message of disdain for others. It is my sincere hope that all users of such tools will uninstall them immediately and refrain from using them in the future.`,
+    `Message loggers: when you like roasting others about their messages so much, you need to keep a record.`,
+  ];
+  const messageText = messageLoggers[Math.floor(Math.random() * messageLoggers.length)];
   const m = await message.channel.send(messageText);
   await m.delete();
 };
