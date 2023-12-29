bba09ffe3712997031e88c5f1928e4d79a5d0da3
Kendell R <KTibow@users.noreply.github.com>
2023-12-29 11:00:40 -0800

    make giveaway monitor better

--- handlers/presence/giveawayMonitor.js
+++ handlers/presence/giveawayMonitor.js
@@ -1,3 +1,6 @@
+/**
+ * @type {Record<string, string[]>}
+ */
 const streaks = {};
 
 /**
@@ -10,28 +13,36 @@ export const command = async ({ content, author, channel, guild, client }) => {
   if (member.roles.cache.has("832754819588292679")) return; // cool people
   if (member.roles.cache.has("832325812329906176")) return; // already has no giveaways
 
-  let streak = streaks[author.id] || 0;
+  let streak = streaks[author.id];
+  if (!streak) {
+    streak = [];
+    streaks[author.id] = streak;
+  }
 
   const words = content.match(/\w+/g);
   if (!words) return;
 
   const isShort = words.length < 3;
   if (isShort) {
-    streak++;
+    streak.push(content);
   } else {
-    streak = 0;
+    streak.length = 0;
   }
-  streaks[author.id] = streak;
 
-  if (streak == 6) {
+  if (streak.length == 6) {
     try {
       member.roles.add("832325812329906176");
+
+      const message = `${author.id} (${author.username}) sent 6 low effort messages in a row, so they were blocked from giveaways`;
+      channel.send(message);
       const verboseBotLogs = /** @type {import("discord.js").TextChannel} */ (
         client.channels.cache.get("934968221923168266")
       );
       if (verboseBotLogs) {
         verboseBotLogs.send(
-          `${author.id} (${author.username}) sent 6 low effort messages in a row, so they were blocked from giveaways`
+          `${message}
+messages:
+${streak.map((m) => `- \`${m}\``).join("\n")}`
         );
       }
     } catch (e) {}
