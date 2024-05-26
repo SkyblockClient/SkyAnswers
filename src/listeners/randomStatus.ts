6da0e40bd5c9231313834a1dbf756e51e708c6f6
RuiNtD <RuiNtD@outlook.com>
2024-05-26 14:44:05 -0600

    Improve status randomizer

--- src/listeners/randomStatus.ts
+++ src/listeners/randomStatus.ts
@@ -2,6 +2,7 @@ import { Events, Listener } from "@sapphire/framework";
 import { type Client, type PresenceStatusData } from "discord.js";
 import { ApplyOptions } from "@sapphire/decorators";
 import { Time } from "@sapphire/time-utilities";
+import { pickRandom } from "@sapphire/utilities";
 
 @ApplyOptions<Listener.Options>({
   once: true,
@@ -10,11 +11,15 @@ import { Time } from "@sapphire/time-utilities";
 export class ReadyListener extends Listener<typeof Events.ClientReady> {
   public override run(client: Client) {
     client.user?.setPresence({ activities: [] });
+    let status: PresenceStatusData = "online";
 
     setInterval(() => {
-      const statuses: PresenceStatusData[] = ["online", "idle", "dnd"];
-      const index = Math.floor(Math.random() * statuses.length);
-      client.user?.setStatus(statuses[index]);
+      status = pickRandom(
+        (["online", "idle", "dnd"] as PresenceStatusData[]).filter(
+          (v) => v != status,
+        ),
+      );
+      client.user?.setStatus(status);
     }, Time.Second * 3);
   }
 }
