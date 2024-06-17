e77e9e8bc7b1a8c90eac8c7b8ea93e1097025c3c
RuiNtD <RuiNtD@outlook.com>
2024-06-17 15:41:09 -0600

    add "why" command

---
+++ src/listeners/resps/why.ts
@@ -0,0 +1,16 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Events, Listener } from "@sapphire/framework";
+import { Message } from "discord.js";
+
+/** Corrects incorrect commands */
+@ApplyOptions<Listener.Options>({
+  event: Events.MessageCreate,
+})
+export class UserEvent extends Listener<typeof Events.MessageCreate> {
+  public override run(message: Message<true>) {
+    const me = message.client.user;
+    const content = message.content.toLowerCase();
+    if (content.startsWith(`${me} why`)) return message.reply("¯\\_(ツ)_/¯");
+    return;
+  }
+}
