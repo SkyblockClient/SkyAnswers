017fdeeebea4bc8f1acdcb0c758dddbfb25e8d86
RuiNtD <RuiNtD@outlook.com>
2024-06-19 15:00:44 -0600

    Add types to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
+// @ts-check
+
 import globals from "globals";
 import js from "@eslint/js";
 import ts from "typescript-eslint";
 import prettier from "eslint-config-prettier";
 
-export default [
+export default ts.config(
   { languageOptions: { globals: globals.node } },
   js.configs.recommended,
   ...ts.configs.recommended,
   prettier,
   { ignores: ["old/", "dist/"] },
-];
+);
