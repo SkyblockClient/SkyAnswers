4350888efa7ee180d0811b4e02ec82dca5fc15b3
RuiNtD <RuiNtD@outlook.com>
2024-06-25 00:59:38 -0600

    Fix logging channel

--- src/lib/logHelper.ts
+++ src/lib/logHelper.ts
@@ -2,7 +2,7 @@ import { ChannelTypes, isDMChannel } from "@sapphire/discord.js-utilities";
 import { User } from "discord.js";
 
 export function formatChannel(channel: ChannelTypes): string {
-  const name = isDMChannel(channel) ? "DM" : `#{channel.name}`;
+  const name = isDMChannel(channel) ? "DM" : `#${channel.name}`;
   return `${name} (${channel.id})`;
 }
 
