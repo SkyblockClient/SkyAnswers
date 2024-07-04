da1b9683a2f2e03180f72105eb828f93b76bdca7
RuiNtD <RuiNtD@outlook.com>
2024-07-04 12:45:03 -0600

    Fix delete response button for Polyfrost

--- src/interaction-handlers/delButton.ts
+++ src/interaction-handlers/delButton.ts
@@ -5,6 +5,7 @@ import {
   container,
 } from "@sapphire/framework";
 import type { ButtonInteraction } from "discord.js";
+import { Polyfrost, SkyClient } from "../const.js";
 
 @ApplyOptions<InteractionHandler.Options>({
   interactionHandlerType: InteractionHandlerTypes.Button,
@@ -13,18 +14,21 @@ export class ButtonHandler extends InteractionHandler {
   public async run(interaction: ButtonInteraction) {
     try {
       if (!interaction.inCachedGuild()) return;
-      const respondedTo = interaction.customId.split("|")[1];
+      const respondedTo = interaction.customId.split("|").at(1);
       if (
-        respondedTo != interaction.member.id &&
-        !interaction.member.permissions.has("ManageMessages") &&
-        !interaction.member.roles.cache.has("931626562539909130") // support person
-      ) {
+        respondedTo == interaction.member.id ||
+        interaction.member.permissions.has("ManageMessages") ||
+        interaction.member.roles.cache.hasAny(
+          SkyClient.roles.SupportTeam,
+          Polyfrost.roles.SupportTeam,
+        )
+      )
+        return interaction.message.delete();
+      else
         return interaction.reply({
-          content: "not your autoresponse",
+          content: "You don't have permission to delete this.",
           ephemeral: true,
         });
-      }
-      return interaction.message.delete();
     } catch (e) {
       container.logger.warn("could not delete", interaction.message, e);
       return interaction.reply({
