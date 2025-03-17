e511b524cd4b1abeddc97b03a4311c6be14c18ce
RuiNtD <RuiNtD@outlook.com>
2025-03-16 22:17:34 -0600

    Update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@
 import globals from "globals";
 import js from "@eslint/js";
 import ts from "typescript-eslint";
-import prettier from "eslint-config-prettier";
+import prettier from "eslint-config-prettier/flat";
 
 export default ts.config(
   { languageOptions: { globals: globals.node } },
